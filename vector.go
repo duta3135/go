14662a24699dbe949f2dcbc918975915c6b1d4c2
duta3135 <dutasetyawan1012@gmail.com>
2022-01-15 11:14:22 +0700

    commit

--- vector.go
+++ vector.go
@@ -18,20 +18,20 @@ func (a Vector) Amplitude() float64 {
 	return math.Sqrt(a.v*a.v + a.w*a.w + a.x*a.x + a.y*a.y + a.z*a.z)
 }
 func main() {
-	var lisia Vector
-	lisia.v = 2
-	lisia.w = 4
-	lisia.x = 4
-	lisia.y = 5
-	lisia.z = 5
+	var duta Vector
+	duta.v = 5
+	duta.w = 1
+	duta.x = 4
+	duta.y = 5
+	duta.z = 4
 	var leri Vector
 	leri.v = 4
 	leri.w = 5
 	leri.x = 3
 	leri.y = 5
 	leri.z = 4
-	dotProduct := Vector{lisia.v, lisia.w, lisia.x, lisia.y, lisia.z}.Dot(Vector{leri.v, leri.w, leri.x, leri.y, leri.z})
-	amplitude1 := Vector{lisia.v, lisia.w, lisia.x, lisia.y, lisia.z}.Amplitude()
+	dotProduct := Vector{duta.v, duta.w, duta.x, duta.y, duta.z}.Dot(Vector{leri.v, leri.w, leri.x, leri.y, leri.z})
+	amplitude1 := Vector{duta.v, duta.w, duta.x, duta.y, duta.z}.Amplitude()
 	amplitude2 := Vector{leri.v, leri.w, leri.x, leri.y, leri.z}.Amplitude()
 	var cosineSim float64 = dotProduct / (amplitude1 * amplitude2)
 	fmt.Printf("kesamaan cosinus antara leri dan lisia: %f", cosineSim)
