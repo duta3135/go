f4452908793a43ec652ddab14c5bfc679730e388
duta3135 <dutasetyawan1012@gmail.com>
2022-01-15 11:30:14 +0700

    commit

--- vector.go
+++ vector.go
@@ -18,21 +18,21 @@ func (a Vector) Amplitude() float64 {
 	return math.Sqrt(a.v*a.v + a.w*a.w + a.x*a.x + a.y*a.y + a.z*a.z)
 }
 func main() {
-	var duta Vector
-	duta.v = 5
-	duta.w = 1
-	duta.x = 4
-	duta.y = 5
-	duta.z = 4
-	var jespan Vector
-	jespan.v = 1
-	jespan.w = 2
-	jespan.x = 5
-	jespan.y = 2
-	jespan.z = 4
-	dotProduct := Vector{duta.v, duta.w, duta.x, duta.y, duta.z}.Dot(Vector{jespan.v, jespan.w, jespan.x, jespan.y, jespan.z})
-	amplitude1 := Vector{duta.v, duta.w, duta.x, duta.y, duta.z}.Amplitude()
-	amplitude2 := Vector{jespan.v, jespan.w, jespan.x, jespan.y, jespan.z}.Amplitude()
+	var ine Vector
+	ine.v = 1
+	ine.w = 1
+	ine.x = 2
+	ine.y = 4
+	ine.z = 1
+	var petrik Vector
+	petrik.v = 5
+	petrik.w = 1
+	petrik.x = 1
+	petrik.y = 1
+	petrik.z = 1
+	dotProduct := Vector{ine.v, ine.w, ine.x, ine.y, ine.z}.Dot(Vector{petrik.v, petrik.w, petrik.x, petrik.y, petrik.z})
+	amplitude1 := Vector{ine.v, ine.w, ine.x, ine.y, ine.z}.Amplitude()
+	amplitude2 := Vector{petrik.v, petrik.w, petrik.x, petrik.y, petrik.z}.Amplitude()
 	var cosineSim float64 = dotProduct / (amplitude1 * amplitude2)
-	fmt.Printf("kesamaan cosinus antara jespan dan duta: %f", cosineSim)
+	fmt.Printf("kesamaan cosinus antara petrik dan ine: %f", cosineSim)
 }
